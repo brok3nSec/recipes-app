b763ca1e665841d9783b8e2ee77545e41f1f4d6e
dreadedDev <dreadeddev@Darions-MacBook-Pro.local>
2019-07-25 11:49:13 -0500

    added hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
-export default class Home extends Component{
-  render(){
-    return(
-      <h1>
-      Home
-      </h1>
-    )
+export default class Home extends Component {
+  render() {
+    return (
+      <Header title="amazing recipes">
+        <Link
+          to="recipes"
+          className="text-uppercase btn 
+      btn-secondary btn-lg mt-3"
+        >
+          seach recipes
+        </Link>
+      </Header>
+    );
   }
 }
